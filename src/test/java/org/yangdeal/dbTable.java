37c87a0098162f9462f2a17d1023d5c72a7e8124
ydeal@msn.com <Yd74hch!>
2018-05-03 23:21:54 +1000

    Combined to one class

---
+++ src/test/java/org/yangdeal/dbTable.java
@@ -0,0 +1,139 @@
+package org.yangdeal;
+
+/*
+Reference:
+https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Java.03.html
+
+start local DynamoDB:
+docker run -v "$PWD":/dynamodb_local_db -p 8000:8000 cnadiminti/dynamodb-local:latest
+
+*/
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+
+public class dbTable {
+
+
+    public static DynamoDB dbConnect() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:8000", "us-west-2"))
+                .build();
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        return dynamoDB;
+    }
+
+    public static void createTable(DynamoDB dbConnect) throws Exception {
+
+        String tableName = "Movies";
+
+        try {
+            System.out.println("Attempting to create table; please wait...");
+            Table table = dbConnect.createTable(tableName,
+                    Arrays.asList(new KeySchemaElement("year", KeyType.HASH), // Partition
+                            // key
+                            new KeySchemaElement("title", KeyType.RANGE)), // Sort key
+                    Arrays.asList(new AttributeDefinition("year", ScalarAttributeType.N),
+                            new AttributeDefinition("title", ScalarAttributeType.S)),
+                    new ProvisionedThroughput(10L, 10L));
+            table.waitForActive();
+            System.out.println("Success.  Table status: " + table.getDescription().getTableStatus());
+
+        } catch (Exception e) {
+            System.err.println("Unable to create table: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    public static void addItem(DynamoDB dbConnect) throws Exception {
+
+        Table table = dbConnect.getTable("Movies");
+
+        int year = 1985;
+        String title = "The Big New Movie";
+
+        final Map<String, Object> infoMap = new HashMap<String, Object>();
+        infoMap.put("plot", "Nothing happens at all.");
+        infoMap.put("rating", 0);
+
+        try {
+            System.out.println("Adding a new item...");
+            PutItemOutcome outcome = table
+                    .putItem(new Item().withPrimaryKey("year", year, "title", title).withMap("info", infoMap));
+
+            System.out.println("PutItem succeeded:\n" + outcome.getPutItemResult());
+
+        } catch (Exception e) {
+            System.err.println("Unable to add item: " + year + " " + title);
+            System.err.println(e.getMessage());
+        }
+
+    }
+
+    public static void deleteTable(DynamoDB dbConnect) throws Exception {
+
+        Table table = dbConnect.getTable("Movies");
+
+        try {
+            System.out.println("Attempting to delete table; please wait...");
+            table.delete();
+            table.waitForDelete();
+            System.out.print("Success.");
+
+        } catch (Exception e) {
+            System.err.println("Unable to delete table: ");
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static String queryTable(DynamoDB dbconnect) throws Exception {
+
+        Table table = dbconnect.getTable("Movies");
+
+        HashMap<String, String> nameMap = new HashMap<String, String>();
+        nameMap.put("#yr", "year");
+
+        HashMap<String, Object> valueMap = new HashMap<String, Object>();
+        valueMap.put(":yyyy", 1985);
+
+        QuerySpec querySpec = new QuerySpec().withKeyConditionExpression("#yr = :yyyy").withNameMap(nameMap)
+                .withValueMap(valueMap);
+
+        ItemCollection<QueryOutcome> items = null;
+        Iterator<Item> iterator = null;
+        Item item = null;
+
+        try {
+            System.out.println("Movies from 1985");
+            items = table.query(querySpec);
+
+            iterator = items.iterator();
+            while (iterator.hasNext()) {
+                item = iterator.next();
+                System.out.println(item.getNumber("year") + ": " + item.getString("title"));
+            }
+
+
+        } catch (Exception e) {
+            System.err.println("Unable to query movies from 1985");
+            System.err.println(e.getMessage());
+        }
+        return item.getString("title");
+    }
+}
--- src/test/java/org/yangdeal/dbTest.java
+++ src/test/java/org/yangdeal/dbTest.java
@@ -1,7 +1,10 @@
 package org.yangdeal;
 
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import org.junit.Assert;
 import org.junit.Test;
+
+import static org.yangdeal.dbTable.dbConnect;
 
 public class dbTest {
 
@@ -9,5 +12,35 @@ public class dbTest {
     public void readFromYaml() {
         config a = new config();
         Assert.assertEquals("ipDb", a.getDbTableName());
+    }
+
+    @Test
+    public void test2() {
+        DynamoDB db = dbConnect();
+        dbTable test = new dbTable();
+        try {
+            test.createTable(db);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            test.addItem(db);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Assert.assertEquals("The Big New Movie", test.queryTable(db));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            test.deleteTable(db);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
